1d1992ec540d716b03b91f77495c9c4350eeba7d
Kiho Han <40457277+kihohan@users.noreply.github.com>
2020-02-26 17:05:45 +0900

    Delete rename_filed.py

--- rename_filed.py
+++
@@ -1,5 +0,0 @@
-# rename_field_name
-import pymongo
-client = pymongo.MongoClient("server")
-db = client.collection_name
-db.field_name.update({}, {'$rename':{'original_name':'new_name'}}, False, True)
