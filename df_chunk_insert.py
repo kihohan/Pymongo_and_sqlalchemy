0acafadd9aab6a48bed90df0f8f637492fa71344
Kiho Han <40457277+kihohan@users.noreply.github.com>
2020-05-15 11:37:38 +0900

    Update df_chunk_insert.py

--- df_chunk_insert.py
+++ df_chunk_insert.py
@@ -1,5 +1,3 @@
-from multiprocessing import Process
-
 def multi_insert_data(df, p_cnt = 10):
 
     def chunks(lst, n):
@@ -10,24 +8,11 @@ def multi_insert_data(df, p_cnt = 10):
     c = round(len(df) / p_cnt)
     df_chunks = list (chunks(df, c))
 
-    a = df_chunks[0]
-    b = df_chunks[1]
-    c = df_chunks[2]
-    d = df_chunks[3]
-    e = df_chunks[4]
-    f = df_chunks[5]
-    g = df_chunks[6]
-    h = df_chunks[7]
-    i = df_chunks[8]
-    j = df_chunks[9]
+    a = df_chunks[0];b = df_chunks[1];c = df_chunks[2];d = df_chunks[3];e = df_chunks[4]
+    f = df_chunks[5];g = df_chunks[6];h = df_chunks[7];i = df_chunks[8];j = df_chunks[9]
 
-    Process(target = insert_data, args=(a,)).start()
-    Process(target = insert_data, args=(b,)).start()
-    Process(target = insert_data, args=(c,)).start()
-    Process(target = insert_data, args=(d,)).start()
-    Process(target = insert_data, args=(e,)).start()
-    Process(target = insert_data, args=(f,)).start()
-    Process(target = insert_data, args=(g,)).start()
-    Process(target = insert_data, args=(h,)).start()
-    Process(target = insert_data, args=(i,)).start()
-    Process(target = insert_data, args=(j,)).start()
+    Process(target = insert_data, args=(a,)).start();Process(target = insert_data, args=(b,)).start()
+    Process(target = insert_data, args=(c,)).start();Process(target = insert_data, args=(d,)).start()
+    Process(target = insert_data, args=(e,)).start();Process(target = insert_data, args=(f,)).start()
+    Process(target = insert_data, args=(g,)).start();Process(target = insert_data, args=(h,)).start()
+    Process(target = insert_data, args=(i,)).start();Process(target = insert_data, args=(j,)).start()
