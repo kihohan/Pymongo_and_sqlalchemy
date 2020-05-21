5c06d151e199939ef23092dbefa3fc970e0f7f30
Kiho Han <40457277+kihohan@users.noreply.github.com>
2020-05-21 17:29:56 +0900

    Update df_chunk_insert.py

--- df_chunk_insert.py
+++ df_chunk_insert.py
@@ -1,5 +1,18 @@
+from multiprocessing import Process
+from pymongo import UpdateOne
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+opr = data.apply(lambda r : UpdateOne({'_id' : r._id}, {'$set':{ 'master_brand_name' : r.master_brand_name }} ), axis=1).to_list()
+opr_lst = [list(x) for x in np.array_split(opr, 6)]
+
+def bulk_insert(opr):
+    client = pymongo.MongoClient("")
+    collection = client.X
+    db = collection.Y
+    db.bulk_write(opr)
+    
+Process(target = bulk_insert, args=(opr_lst[0],)).start()
+Process(target = bulk_insert, args=(opr_lst[1],)).start()
+Process(target = bulk_insert, args=(opr_lst[2],)).start()
+Process(target = bulk_insert, args=(opr_lst[3],)).start()
+Process(target = bulk_insert, args=(opr_lst[4],)).start()
+Process(target = bulk_insert, args=(opr_lst[5],)).start()
